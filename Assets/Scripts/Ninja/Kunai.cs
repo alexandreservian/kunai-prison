7d9ea59707b40e279e17c3cc379097c626d0c5ce
alexandreservian <alexandreservian@gmail.com>
2024-02-25 18:57:24 -0300

    refactor: ajustando limites para a kunai

--- Assets/Scripts/Ninja/Kunai.cs
+++ Assets/Scripts/Ninja/Kunai.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Kunai : MonoBehaviour
 {
     private Camera mainCamera;
     private Vector3 mousePos;
+    [Header("Configuração da kunai")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float force;
+    [SerializeField] private float limitHorizontal = 1;
+    [SerializeField] private float limitVertical = 1;
+    private Vector2 screenBounds;
 
     void OnEnable()
     {
@@ -16,10 +21,23 @@ public class Kunai : MonoBehaviour
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
         var rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ + 90);
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
     }
 
     void Update()
     {
-        
+        if (transform.position.x > screenBounds.x || transform.position.x < -screenBounds.x ||
+            transform.position.y > screenBounds.y || transform.position.y < -screenBounds.y)
+        {
+            gameObject.SetActive(false);
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.CompareTag("Obstacle")) {
+            gameObject.SetActive(false);
+        }
+    }
+    
+
 }
