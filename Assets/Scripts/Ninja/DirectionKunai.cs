cf8ff3240e248bb714858be07b78b1fa4e23a729
alexandreservian <alexandreservian@gmail.com>
2024-02-25 16:02:30 -0300

    feat: criando teleport com a kunai

--- Assets/Scripts/Ninja/DirectionKunai.cs
+++ Assets/Scripts/Ninja/DirectionKunai.cs
@@ -4,6 +4,9 @@ public class DirectionKunai : MonoBehaviour
 {
     private Camera mainCamera;
     private Vector3 mousePos;
+    [SerializeField] private Transform ninja;
+    [SerializeField] private GameObject kunai;
+    [SerializeField] private Transform kunaiOrigins;
     void Start()
     {
         mainCamera = Camera.main;
@@ -16,6 +19,16 @@ public class DirectionKunai : MonoBehaviour
         var rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        if(Input.GetMouseButtonDown(0) && !kunai.activeSelf) {
+            kunai.SetActive(true);
+            kunai.transform.position = kunaiOrigins.position;
+        }
+
+        if(Input.GetMouseButtonDown(1) && kunai.activeSelf) {
+            kunai.SetActive(false);
+            ninja.position = kunai.transform.position;
+        }
     }
 }
    
---
+++ Assets/Scripts/Ninja/Kunai.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Kunai : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 mousePos;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float force;
+
+    void OnEnable()
+    {
+        Debug.Log("Start na kunai");
+        mainCamera = Camera.main;
+        mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        var direction = mousePos - transform.position;
+        var rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        var rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ + 90);
+    }
+
+    void Update()
+    {
+        
+    }
+}
